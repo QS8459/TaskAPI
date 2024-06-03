e85fe536260d7451eee7c8154e56a06f8c59bec2
User <sardorqoshnazarov@novalab.uz>
2024-06-03 22:55:15 +0500

    smal changes in t.py

--- t.py
+++ t.py
@@ -1,14 +1,43 @@
-from contextlib import asynccontextmanager;
+from contextlib import asynccontextmanager
 
-from fastapi import FastAPI, APIRouter, HTTPException;
-from src.api import router;
-from starlette.responses import JSONResponse;
-app = FastAPI();
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from src.api import router
+from src.config import settings
+from src.dependencies import init_dependencies
 
 
 @asynccontextmanager
 async def lifespan(_app: FastAPI):
+    # you can do some initialization here
     yield
 
 
-app.include_router(router = router);
+def init_routers(_app: FastAPI):
+    app.include_router(router)
+
+
+app = FastAPI(
+    title=settings.app_title,
+    description=settings.app_description,
+    version=settings.app_version,
+    lifespan=lifespan,
+)
+app.include_router(router)
+init_dependencies(app)
+
+@app.get('/home')
+async def home():
+    from starlette.responses import JSONResponse
+    return JSONResponse("hello world");
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
