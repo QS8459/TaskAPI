749ddb14fbf3416be7767bb6743d686d0d4d9ded
QS8459 <sardorgpt@gmail.com>
2025-01-07 21:54:19 +0500

    BaseService and services

--- src/core/service/account/account.py
+++ src/core/service/account/account.py
@@ -9,67 +9,14 @@ from src.ver_email import email_server, Email;
 
 from src.db.models import Account;
 from src.db.engine import get_async_session
-from src.core.service import AbstractBaseService;
+from src.core.service.base import BaseService;
 from src.core.schemas.account import AccountLoginSchema;
 
-class AccountService(AbstractBaseService):
+class AccountService(BaseService):
 
     def __init__(self, session:AsyncSession):
         super().__init__(session, Account)
 
-    async def add_account(self, **kwargs) -> Account:
-        try:
-            async with self.session:
-                password = kwargs.pop("password")
-                instance = self.model(**kwargs);
-                instance.set_password(password);
-                self.session.add(instance);
-                await self.session.commit();
-                await self.session.refresh(instance);
-                email_server(Email(
-                    subject="Verify Your Email",
-                    body=f"http://127.0.0.1:8000/api/v1/account/verify_account/{instance.id}/",
-                    recipient=instance.email
-                ));
-                return instance;
-        except SQLAlchemyError as e:
-            raise e;
-
-    async def get_by_email(self, email:str):
-        try:
-            async with self.session:
-                query = select(self.model).where(self.model.email == email);
-                result = await self.session.execute(query);
-                instance = result.scalars().first();
-                if not instance:
-                    raise ValueError(f"{self.model.__name__} not found")
-                return instance;
-        except SQLAlchemyError as e:
-            raise e;
-
-    async def auth_user(self, credential:AccountLoginSchema):
-        try:
-            async with self.session:
-                account = await self.get_by_email(credential.email);
-                if not account:
-                    raise ValueError(f"{self.model.__name__} not found");
-                if not account.verify_password(credential.password):
-                    raise ValueError(f"{self.model.__name__} Invalid Password")
-                if account.is_active == False:
-                    raise ValueError(f"{self.model.__name__} not found")
-                return account;
-        except SQLAlchemyError as e:
-            raise e;
-
-    async def activate_user(self, id_:UUID):
-        try:
-            async with self.session:
-                account = await super().get_and_update(id_, is_active = True);
-                if not account:
-                    raise ValueError("Something went wrong in", account.__name__);
-                return account;
-        except SQLAlchemyError as e:
-            raise e;
 
 def get_account_service(session:AsyncSession = Depends(get_async_session)) -> AccountService:
     return AccountService(session);
--- src/core/service/task/task.py
+++ src/core/service/task/task.py
@@ -3,7 +3,7 @@ from typing import Annotated;
 from fastapi import Depends;
 from src.db.engine import get_async_session;
 from sqlalchemy.ext.asyncio import AsyncSession;
-from src.core.service.base import AbstractBaseService;
+from src.core.service.base import BaseService;
 from sqlalchemy.future import select;
 from src.db.models.task.task import Task;
 from sqlalchemy.exc import SQLAlchemyError;
@@ -12,37 +12,10 @@ from sqlalchemy import func, asc, desc;
 from src.core.schemas.account import AccountDetail;
 from src.pagination import Pagination, pagination_param
 
-class TaskService(AbstractBaseService):
+class TaskService(BaseService):
 
     def __init__(self, session:AsyncSession):
         super().__init__(session, Task);
-
-    async def add_task(self, user, **kwargs):
-        kwargs['created_by'] = user.id;
-        return await super().create(**kwargs);
-
-    async def get_all_task(self, user: AccountDetail, pagination: Annotated[Pagination, Depends(pagination_param)]):
-        try:
-            async with self.session:
-                query = (
-                    select(self.model).filter(self.model.created_by == user.id)
-                    .limit(pagination.perPage)
-                    .offset(
-                        pagination.page - 1
-                        if pagination.page == 1
-                        else     (pagination.page - 1) * pagination.perPage
-                    )
-                );
-
-                result = await self.session.execute(query.order_by(asc(self.model.created_at)));
-
-                task = result.scalars().all();
-                count_query = select(func.count()).select_from(self.model).filter(self.model.created_by == user.id)
-                result_c = await self.session.execute(count_query);
-                count = result_c.scalar();
-                return {'task':task, 'count':count}
-        except SQLAlchemyError as e:
-            raise e;
 
 
 def get_task_service(session: AsyncSession = Depends(get_async_session)) -> AsyncSession:
