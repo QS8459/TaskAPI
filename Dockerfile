c340a2f9809aed18bc72989d75f9ceb5ecca076f
QS8459 <sardorgpt@gmail.com>
2025-01-13 02:15:25 +0500

    Fixes with initializtion

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,11 @@ FROM python:3.11-alpine
 
 WORKDIR /app
 
-COPY requirements.txt ./
+COPY . .
 
 RUN pip install -r requirements.txt
 
+# Enable systemd service
 ENV TK_POSTGRES_USER=fastapi
 ENV TK_POSTGRES_DB=taskapi
 ENV TK_POSTGRES_PASSWORD=fastapi_admin
@@ -16,7 +17,7 @@ ENV TK_APP_DESCRIPTION=Application_for_Task_management
 ENV TK_APP_VERSION=0.0.1
 ENV TK_PG_URI=postgresql+asyncpg://${TK_POSTGRES_USER}:${TK_POSTGRES_PASSWORD}@${TK_POSTGRES_HOST}:${TK_POSTGRES_PORT}/${TK_POSTGRES_DB}
 
-COPY . .
-
 EXPOSE 7000
+
+
 
--- src/api/v1/user/user.py
+++ src/api/v1/user/user.py
@@ -2,48 +2,42 @@ from uuid import UUID
 from fastapi import APIRouter, Depends
 
 from src.core.service.account.account import account_service
-from src.core.schemas.account import AccountSignInSchema, CustomAuthForm, AccountResponseSchema
+from src.core.schemas.account import (
+    AccountSignInSchema,
+    CustomAuthForm,
+    AccountResponseSchema,
+)
 from src.core.schemas.token import TokenSchemaBase
 
+user_api = APIRouter(prefix="/user")
 
 
-user_api = APIRouter(prefix='/user')
-
-
-@user_api.post('/sign_in/')
-async def sing_in(
-        data: AccountSignInSchema,
-        service=Depends(account_service)
-):
-    instance = await service.add(**data.dict());
+@user_api.post("/sign_in/")
+async def sing_in(data: AccountSignInSchema, service=Depends(account_service)):
+    instance = await service.add(**data.dict())
     return instance
 
 
-@user_api.get('/{id}', status_code=200)
-async def get_user(
-        id: UUID,
-        service=Depends(account_service)
-):
+@user_api.get("/{id}", status_code=200)
+async def get_user(id: UUID, service=Depends(account_service)):
     instance = await service.get_by_id(id=id)
     return instance
 
-@user_api.get('/{email}/')
-async def get_user_by_email(
-        email: str,
-        service = Depends(account_service)
-):
+
+@user_api.get("/{email}/")
+async def get_user_by_email(email: str, service=Depends(account_service)):
     instance = await service.get_by_email(email)
     return instance
 
-@user_api.post('/login/', response_model = TokenSchemaBase)
+
+@user_api.post("/login/", response_model=TokenSchemaBase)
 async def login(
-        form_data: CustomAuthForm = Depends(),
-        service = Depends(account_service)
+    form_data: CustomAuthForm = Depends(), service=Depends(account_service)
 ):
     response = await service.auth_user(form_data.username, form_data.password)
     return response
-@user_api.get('/me/', response_model = AccountResponseSchema)
-async def me(
-        user: AccountSignInSchema = Depends(account_service().get_current_user)
-):
+
+
+@user_api.get("/me/", response_model=AccountResponseSchema)
+async def me(user: AccountSignInSchema = Depends(account_service().get_current_user)):
     return user
